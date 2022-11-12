edee8e7e7b5cb76e9d87ae3b8dad81c2211476c4
Ulan <kozhabekov.u@gmail.com>
2022-11-12 22:31:04 +0600

    Preparing for multiple cross dev

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,26 @@
 import React, {Component} from 'react';
+import HomePage from "./page/HomePage";
+import ProductsPage from "./page/ProductsPage";
 
 class Main extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <>
+                {/*
+                Nursat component
+                <HomePage/>
+                */}
+
+                {/*
+                Ulan component
+                <ProductPage/>
+                */}
+
+                {/*
+                Jaras component
+                <AboutPage/>
+                */}
+            </>
         );
     }
 }
