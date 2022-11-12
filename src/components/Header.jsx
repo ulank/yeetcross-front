75b2e4c62beabbc7b54a3af46a572da69cf346b5
Ulan <kozhabekov.u@gmail.com>
2022-11-13 00:10:21 +0600

    Dialog

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,32 @@
 import React, {Component} from 'react';
 import Navbar from "./shared/Navbar";
+import {NavLink} from "react-router-dom";
+import Dialog from "./shared/Dialog";
 
 class Header extends Component {
+    state = {
+        isOpen: false
+    }
     render() {
         return (
-            <header className="header-container">
-                <a className="logo" href="/">
-                    yeetcross
-                </a>
-                <Navbar/>
-                <button className="sign-btn">
-                    Sign In
-                </button>
-            </header>
+            <>
+                <header className="header-container">
+                    <NavLink className="logo" to="/">
+                        yeetcross
+                    </NavLink>
+                    <Navbar/>
+                    <button className="sign-btn" onClick={() => this.setState({ isOpen: true })}>
+                        Sign In
+                    </button>
+                </header>
+                <Dialog isOpen={this.state.isOpen} onClose={() => this.setState({ isOpen: false })}>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste a ipsam repellendus commodi ad, fugit id magnam inventore laudantium autem delectus praesentium incidunt debitis, numquam dicta eveniet obcaecati, itaque quidem?
+                </Dialog>
+            </>
+
         );
     }
 }
 
 export default Header;
+
